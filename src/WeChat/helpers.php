c8d8b9744acf6b2c78b35f8d5a3ed10dcc2a1cd7
linyl <service@im050.com>
2017-04-25 11:44:23 +0800

    增加重名判断

--- src/WeChat/helpers.php
+++ src/WeChat/helpers.php
@@ -1,35 +1,93 @@
 <?php
+/*
+ * 自定义快捷函数
+ */
 
-function http() {
-    static $http_client = null;
-    if ($http_client == null) {
-        $http_client = new \Im050\WeChat\Component\HttpClient();
-    }
-    return $http_client;
-}
-
-function app() {
-    return \Im050\WeChat\Core\Application::getInstance();
-}
-
-function uri($name) {
-    static $servers = [
-        'base_uri' => 'https://wx.qq.com',
-        'login_uri' => 'https://login.wx.qq.com',
-        'push_uri' => 'https://webpush.wx.qq.com'
-    ];
-    if (isset($servers[$name])) {
-        return $servers[$name];
-    } else {
-        return null;
+if (!function_exists('http')) {
+    /**
+     * 获取HTTP CLIENT
+     *
+     * @return \Im050\WeChat\Component\HttpClient|null
+     */
+    function http()
+    {
+        static $http_client = null;
+        if ($http_client == null) {
+            $http_client = new \Im050\WeChat\Component\HttpClient();
+        }
+        return $http_client;
     }
 }
 
-function checkBaseResponse($data) {
-    return intval($data['BaseResponse']['Ret']) == 0;
+if (!function_exists('app')) {
+    /**
+     * 获取应用容器
+     *
+     * @return \Im050\WeChat\Core\Application|null
+     */
+    function app()
+    {
+        return \Im050\WeChat\Core\Application::getInstance();
+    }
 }
 
-function config($param) {
-    $config = \Im050\WeChat\Component\Config::getInstance();
-    return $config->get($param);
+if (!function_exists('uri')) {
+    /**
+     * 获取请求地址
+     *
+     * @param $name
+     * @return mixed|null
+     */
+    function uri($name)
+    {
+        static $servers = [
+            'base_uri'  => 'https://wx.qq.com',
+            'login_uri' => 'https://login.wx.qq.com',
+            'push_uri'  => 'https://webpush.wx.qq.com'
+        ];
+        if (isset($servers[$name])) {
+            return $servers[$name];
+        } else {
+            return null;
+        }
+    }
+}
+
+if (!function_exists('checkBaseResponse')) {
+    /**
+     * 校验数据是否正确返回
+     *
+     * @param $data
+     * @return bool
+     */
+    function checkBaseResponse($data)
+    {
+        return intval($data['BaseResponse']['Ret']) == 0;
+    }
+}
+
+if (!function_exists('members')) {
+    /**
+     * 获取联系人列表
+     *
+     * @return \Im050\WeChat\Collection\Members|null
+     */
+    function members()
+    {
+        return \Im050\WeChat\Collection\Members::getInstance();
+    }
+}
+
+if (!function_exists('config')) {
+    /**
+     * 获取配置
+     *
+     * @param $param
+     * @return mixed|null
+     */
+    function config($param)
+    {
+        $config = \Im050\WeChat\Component\Config::getInstance();
+        return $config->get($param);
+    }
 }
