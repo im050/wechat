bd887756d994216e5b2d888f61a3acd17e9c8892
linyl <service@im050.com>
2019-04-12 02:04:35 +0800

    refactor: 例子调整

--- example/example.php
+++ example/example.php
@@ -3,20 +3,23 @@ define('BASE_PATH', dirname(dirname(__FILE__)));
 
 include(BASE_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-use Im050\WeChat\Component\Console;
-use Im050\WeChat\Message\Formatter\Message;
 use Im050\WeChat\Collection\Element\Contact;
 use Im050\WeChat\Collection\Element\Group;
 use Im050\WeChat\Collection\Element\Official;
+use Im050\WeChat\Component\Console;
 use Im050\WeChat\Core\Account;
-use Im050\WeChat\Message\MessageFactory;
-use Im050\WeChat\Task\TaskQueue;
+use Im050\WeChat\Message\Formatter\Message;
+use Im050\WeChat\Message\Formatter\NewFriend;
+use Im050\WeChat\Message\Formatter\Recalled;
+use Im050\WeChat\Message\Formatter\RedPacket;
+use Im050\WeChat\Message\Formatter\Text;
+use Im050\WeChat\Message\Formatter\Transfer;
 
 $robot = new \Im050\WeChat\Core\Robot([
     //临时文件夹
     'tmp_path'         => BASE_PATH . DIRECTORY_SEPARATOR . 'tmp',
     //日志级别
-    'log_level' => \Monolog\Logger::INFO,
+    'log_level'        => \Monolog\Logger::DEBUG,
     //下载二维码
     'save_qrcode'      => false,
     //自动下载图片、语音、视频等资源
@@ -45,28 +48,36 @@ $robot->onLoginSuccess(function () {
 /**
  * 定时任务
  */
-$robot->cron("*/1 * * *", function() {
+$robot->cron("*/1 * * * *", function () {
     $fileHelper = members()->getContactByUserName('filehelper');
     //第二个参数以阻塞方式发送消息
     $fileHelper->sendMessage("hello! " . time(), true);
 });
 
+$robot->onLogout(function ($code) {
+    //code=1100 or 1101 在其他客户端登录
+    //code=1102 无效的cookies
+    //code=1105 api频率限制
+    Console::log("退出登录了");
+});
+
 /**
  * 消息事件回调
  */
-$robot->onMessage(function(Message $message, $robot){
-
-    //获取消息类型
-    $messageType = $message->getMessageType();
+$robot->onMessage(function (Message $message) {
     //判断消息类型
-    switch($messageType) {
-        case MessageFactory::TEXT_MESSAGE: //equals "$message instanceof Text";
-            Console::log("文本消息");
-            break;
-        case MessageFactory::VOICE_MESSAGE:
-            Console::log("语音消息");
-            break;
-        //and so on...
+    if ($message instanceof Text) {
+        Console::log("文本消息");
+    } elseif ($message instanceof RedPacket) {
+        Console::log("是个红包消息");
+    } elseif ($message instanceof NewFriend) {
+        $message->approve(); //通过好友验证
+    } elseif ($message instanceof Recalled) {
+        $message->backup(); //备份撤回的消息
+    } elseif ($message instanceof Transfer) {
+        Console::log("转账消息，转账金额" . $message->getFee());
+    } else {
+        Console::log("其他资源消息，" . $message->getMessageType());
     }
 
     //获取信息发送者
@@ -97,29 +108,28 @@ $robot->onMessage(function(Message $message, $robot){
     if ($receiver->getUserName() == Account::username()) {
         Console::log("这是一条发给我的消息");
     } else {
-        //可能是条群消息
+        Console::log("可能是条群消息");
     }
 
     //给发信人回复消息
     $file = '文件名';
     //文字消息
-    $messenger->sendMessage("文字消息");
+    //$messenger->sendMessage("文字消息");
     //动画表情消息
-    $messenger->sendEmoticon($file);
+    //$messenger->sendEmoticon($file);
     //图片消息
-    $messenger->sendImage($file);
+    //$messenger->sendImage($file);
     //传输文件
-    $messenger->sendFile($file);
+    //$messenger->sendFile($file);
 
-    //任务队列
-    $job = 'RobotReply'; //内置的图灵机器人回复任务, 也可以写成 RobotReply::class
     //任务需要的参数
     $params = [
         'username'     => $messenger->getUserName(),
         'from_message' => $message->string(),
         'userid'       => md5($messenger->getUserName())
     ];
-    TaskQueue::run($job, $params); //equals "app()->task_queue->task($job, $params)";
+    //通过队列运行任务
+    //app()->taskQueue->task(RobotReply::class, $params);
 });
 
 $robot->run();
